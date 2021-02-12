13b1af7fa53c797636961d82c002668672d75ca1
adybka <adybka10@gmail.com>
2021-02-12 10:31:29 -0500

    - fix tests

--- src/test/java/com/example/lab3/AppTests.java
+++ src/test/java/com/example/lab3/AppTests.java
@@ -4,20 +4,19 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.junit.Assert.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -40,7 +39,7 @@ public class AppTests {
             .andDo(print())
             .andExpect(status().isOk());
         assertEquals(this.addressBookRepoRepo.count(), 1);
-        assertEquals(this.buddyRepo.count(), 2); 
+        assertEquals(this.buddyRepo.count(), 2);
     }
 
     @Test
